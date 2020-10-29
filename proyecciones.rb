ecb0d81eccf1c79f8d07c1805347b4e7a16e0f6c
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-29 09:13:17 -0300

    logramos meter en un nuevo arreglo los primeros 6 elementos aumentados por 10%

--- proyecciones.rb
+++ proyecciones.rb
@@ -1,7 +1,5 @@
 =begin 
 Pasos a seguir:
-1) tomar datos de archivo externo
-2) convertir en arreglo
 3) crear un nuevo arreglo con los primeros 6 elementos aumentados un 10%
 4) sumar los elementos del arreglo
 5) Repetir de 1 a 4, pero en paso dos, en vez de un 10% a los primeros 6, se debe aumentar un 20% a los ultimos 6
@@ -13,8 +11,16 @@ If we want to display floating point numbers we need to use %f. We can specify t
 
 
 sales_string = open('ventas_base.db').read.chomp.split(',') #tomamos datos de archivo externo y convertimos en arreglo
-sales_integer = sales_string.map { |x| x.to_i}
+sales_integer = sales_string.map { |x| x.to_i } #son integers ahora
+new_sales = []
+for i in (0..5)
+    new_sales.push (sales_integer[i] * 1.1)
+end
 
+
+
+
+print new_sales
 
 
 
