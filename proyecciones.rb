5c81954d024538e75f41b4f8b608dcddfad7fb72
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-28 22:24:50 -0300

    hicimos un pequeño diagrama

---
+++ proyecciones.rb
@@ -0,0 +1,12 @@
+=begin 
+Pasos a seguir:
+1) tomar datos de archivo externo
+2) convertir en arreglo
+3) crear un nuevo arreglo con los primeros 6 elementos aumentados un 10%
+4) sumar los elementos del arreglo
+5) Repetir de 1 a 4, pero en paso dos, en vez de un 10% a los primeros 6, se debe aumentar un 20% a los ultimos 6
+6) imprimir los 2 totales en un nuevo archivo con máximo 2 decimales
+
+If we want to display floating point numbers we need to use %f. We can specify the number of decimal places we want like this: %0.2f.
+=end
+
