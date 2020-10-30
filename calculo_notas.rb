efd6bd33947f02523e468d72f42ad9f633f1b0bb
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-29 21:16:44 -0300

    avanzando

--- calculo_notas.rb
+++ calculo_notas.rb
@@ -1,2 +1,16 @@
 grades = open('notas.data').readlines
-print grades
+student_lenght = []
+student_lenght = grades[0].split(',')
+n = student_lenght.size
+
+first_student = []
+
+n.times do |i|
+    if i == 0
+        first_student.push(student_lenght[i].to_s)
+    else
+        first_student.push(student_lenght[i].to_i)
+    end
+end
+
+print first_student
